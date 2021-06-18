bc06274ba9b3845f4bfa87f7d4283d510f7f27fe
AngieCLongo <86113951+AngieCLongo@users.noreply.github.com>
2021-06-18 13:26:49 -0300

    Update SumTwoNumbers.java
    Cambié null por a +b. Por lo que el resultado de la función Sum va a ser a + b

--- src/main/java/com/redbee/academy/challenge/SumTwoNumbers.java
+++ src/main/java/com/redbee/academy/challenge/SumTwoNumbers.java
@@ -11,7 +11,7 @@ public class SumTwoNumbers {
    * @return The result of a + b
    */
   public static Integer sum(Integer a, Integer b) {
-    //TODO: Implement me
-    return null;
+    
+    return a + b ;
   }
 }
